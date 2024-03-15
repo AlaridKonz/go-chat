6f9ec78d12dbf6ae207749f14b17f4bb444f98b7
Jose Alarid <alarid.konz@gmail.com>
2024-03-14 20:55:10 -0700

    First implementation of a Chat Message wrapper

---
+++ internal/models/structs/ChatNotifications.go
@@ -0,0 +1,45 @@
+package structs
+
+import (
+	"encoding/json"
+	"gochat/internal/models/payloads"
+	ds "gochat/internal/utils/datastructures"
+	ws "gochat/internal/utils/websocket"
+
+	"github.com/gorilla/websocket"
+)
+
+type ChatNotification struct {
+	ChatMessage          payloads.Chat
+	UnnotifiedRecipients *ds.XSet[int]
+}
+
+func NewChatNotification(chat payloads.Chat) *ChatNotification {
+	return &ChatNotification{
+		ChatMessage:          chat,
+		UnnotifiedRecipients: ds.NewXSet[int](chat.Meta.Members...),
+	}
+}
+
+func (cn *ChatNotification) Notified(recipient int) {
+	cn.UnnotifiedRecipients.Remove(recipient)
+}
+
+func (cn *ChatNotification) HasNotifiedAll() bool {
+	return cn.UnnotifiedRecipients.IsEmpty()
+}
+
+func (cn *ChatNotification) NotifyUser(conn *websocket.Conn, recipientid int) error {
+	if !cn.UnnotifiedRecipients.Contains(recipientid) {
+		return nil
+	}
+	jsonStr, err := json.Marshal(cn.ChatMessage)
+	if err != nil {
+		return err
+	}
+	if err := ws.Notify(conn, jsonStr); err != nil {
+		return err
+	}
+	cn.Notified(recipientid)
+	return nil
+}
